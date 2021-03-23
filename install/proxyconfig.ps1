3c1af041ab94b938378fa7f013b4c3f176472e94
Matt Hansen <mahans@microsoft.com>
2021-03-23 06:59:57 -0400

    v1.0

--- install/proxyconfig.ps1
+++ install/proxyconfig.ps1
@@ -1,3 +1,4 @@
+mkdir C:\proxyconfig
 Invoke-WebRequest -URI "https://raw.githubusercontent.com/matthansen0/azure-gatesentry-proxy/main/install/proxyclient.ps1" -OutFile C:\proxyconfig\proxyclient.ps1
 
 $RunOnceKey = "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce"
